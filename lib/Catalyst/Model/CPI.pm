56a3f97c1c22d1a8193b8ef431fc8ef3ed0d770d
André Walker <andre@andrewalker.net>
2013-01-10 20:38:25 -0200

    Add initial code

--- lib/Catalyst/Model/CPI.pm
+++ lib/Catalyst/Model/CPI.pm
@@ -1,4 +1,100 @@
 package Catalyst::Model::CPI;
+use Moose;
+use Module::Pluggable (
+    search_path => [ 'Business::CPI::Gateway' ],
+    require     => 1,
+);
+use Moo::Role ();
+
+extends 'Catalyst::Model';
+
+has config_for_gateway => (
+    isa     => 'HashRef',
+    is      => 'ro',
+    default => sub { +{} },
+    traits  => ['Hash'],
+    handles => {
+        get_config_for_gateway => 'get',
+    },
+);
+
+has _req => ( is => 'rw' );
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $args = $self->$orig(@_);
+
+    $args->{config_for_gateway} = delete $args->{gateway};
+
+    return $args;
+};
+
+sub COMPONENT {
+    my ($self, $ctx) = @_;
+
+    for ($self->plugins) {
+        Moo::Role->apply_roles_to_package(
+            $_, 'Business::CPI::Role::Request'
+        );
+    }
+}
+
+sub ACCEPT_CONTEXT {
+    my ($self, $ctx) = @_;
+
+    $self->_req($ctx->req);
+
+    return $self;
+}
+
+sub get {
+    my ($self, $name) = @_;
+
+    if (!$self->exists($name)) {
+        local $" = ", ";
+        my @plugins = $self->plugins;
+        die "Can't get gateway $name. Available gateways are @plugins";
+    }
+
+    my $fullname = "Business::CPI::Gateway::$name";
+
+    my %args = %{ $self->get_config_for_gateway($gateway) };
+    $args{req} = $self->_req;
+
+    return $fullname->new(%args);
+}
+
+sub exists {
+    my ($self, $name) = @_;
+
+    my $fullname = "Business::CPI::Gateway::$name";
+
+    for ($self->plugins) {
+        return 1 if $_ eq $fullname;
+    }
+
+    return 0;
+}
+
+package # hide from PAUSE
+    Business::CPI::Role::Request;
+use Moo::Role;
+
+has req => ( is => 'ro' );
+
+around notify => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    if (scalar @_) {
+        die "You are using Business::CPI from Catalyst.\n" .
+            "You don't have to pass the request!\n";
+    }
+
+    return $self->$orig($self->req);
+};
 
 1;
 
